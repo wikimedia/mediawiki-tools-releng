2883d09c461b4bd3a749965c63fe3896674e9330
Antoine Musso <hashar@free.fr>
2014-03-04 00:07:10 +0100

    Actually apply the nature stylesheet
    The releng.css was always including defaults.css which is the default
    theme and not the nature one :-/
    
    Change-Id: Ib10818eae92f0ad10293da82a16c0738599061b8

--- source/conf.py
+++ source/conf.py
@@ -100,6 +100,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+#
+# WARNING: please also update the file source/_static/releng.css
 html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
